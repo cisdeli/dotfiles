d8f874dd44ce4daa9b018b291279a267a1b8a0b2
cisdeli <pedrohcisdeli@gmail.com>
2024-02-10 00:38:48 -0600

    Added support to web dev languages

--- init.lua
+++ init.lua
@@ -69,7 +69,7 @@ vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
 -- Treesitter
 local config = require("nvim-treesitter.configs")
 config.setup({
-    ensure_installed = {"lua", "python", "c", "cpp", "javascript", "typescript", "css"},
+    ensure_installed = {"lua", "python", "c", "cpp", "javascript", "typescript", "css", "r", "html" },
     highlight = {enable = true},
     indent = {enable = true},
 })

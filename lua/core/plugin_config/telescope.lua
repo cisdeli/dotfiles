d6c2d7f951314b14282dec432420511428694eb1
cisdeli <pedrohcisdeli@gmail.com>
2024-02-15 23:39:25 -0600

    Added telescope ui ext

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -3,3 +3,5 @@ require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+require("telescope").load_extension("ui-select")

425527192cc70fbec9621be6225131325d495631
cisdeli <pedrohcisdeli@gmail.com>
2024-02-25 23:46:53 -0600

    Changed telescope setup to find hidden files

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,5 +1,26 @@
 -- Telescope mappings
-require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            find_command = {
+                "rg",
+                "--no-ignore",
+                "--hidden",
+                "--files",
+                "-g",
+                "!**/node_modules/*",
+                "-g",
+                "!**/.git/*",
+            },
+        },
+    },
+    extensions = {
+        file_browser = {
+            hidden = { file_browser = true, folder_browser = true },
+            prompt_path = true,
+        },
+    },
+})
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})

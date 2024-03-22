43ff9f1420b0031e45075ba07c4525bd1fb35040
cisdeli <pedrohcisdeli@gmail.com>
2024-03-22 10:05:35 -0500

    Making main usable for both systems

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -17,13 +17,10 @@ require('telescope').setup({
                 "!**/target/debug/*",
                 "-g",
                 "!**/CMakeFiles/*",
-<<<<<<< HEAD
-=======
                 "-g",
                 "!**/.cache/*",
                 "-g",
                 "!**/*.out",
->>>>>>> 822ba7eb77ae7946020d1c73f540f455c1923e3f
             },
         },
     },

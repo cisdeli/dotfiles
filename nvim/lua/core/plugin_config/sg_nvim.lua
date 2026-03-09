050e6668ee04116179913d26735a999a09e9daf3
cisdeli <pedrohcisdeli@gmail.com>
2026-03-09 03:16:35 -0400

    chore: remove unused plugins

--- nvim/lua/core/plugin_config/sg_nvim.lua
+++
@@ -1,8 +0,0 @@
-local lsp_zero = require('lsp-zero')
-require("sg").setup {
-  -- Pass your own custom attach function
-  --    If you do not pass your own attach function, then the following maps are provide:
-  --        - gd -> goto definition
-  --        - gr -> goto references
-  on_attach = lsp_zero.on_attach
-}

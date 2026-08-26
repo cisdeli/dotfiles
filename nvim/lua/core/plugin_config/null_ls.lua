5801d6e83a03fd74664d58dc4d905b1de5b34f30
cisdeli <pedrohcisdeli@gmail.com>
2026-08-26 16:17:27 -0400

    chore: add suport for other filetypes with clang format

--- nvim/lua/core/plugin_config/null_ls.lua
+++ nvim/lua/core/plugin_config/null_ls.lua
@@ -4,7 +4,9 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.clang_format.with({
+            filetypes = { "c", "cpp", "cs", "cuda", "proto", "objc", "objcpp", "glsl" },
+        }),
         null_ls.builtins.formatting.prettier,
     },
 })
